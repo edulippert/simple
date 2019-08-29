9551eb68edbbf4d1ff3369a787dd54a5f46063e8
Sergio Cuadros <sergiocua2@gmail.com>
2019-08-29 01:01:28 -0300

    Added logic to condominium model

--- app/Condominium.php
+++ app/Condominium.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Condominium extends Model
 {
-    //
+    protected $fillable = [
+        'company_id',
+        'file_id',
+        'name',
+        'id_number',
+        'address',
+        'complement',
+        'zipcode',
+        'licence_due_date',
+        'is_active'
+    ];
+
+    protected $guarded = ['id'];
+
+    protected $table = 'condominiums';
+
+    public function companies() {
+        return $this->belongsTo(company::class);
+    }
+
+    public function files() {
+        return $this->hasMany(File::class);
+    }
 }
