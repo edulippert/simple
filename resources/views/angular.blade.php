a0b4b9381bccbd4526b64abc2265ce5dca96b497
Willian <willian@codwise.com>
2019-10-14 01:23:56 -0300

    FIxing problemns with deploy

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -14,6 +14,6 @@
 <script type="text/javascript" src="/js/runtime.26209474bfa8dc87a77c.js"></script>
 <script type="text/javascript" src="/js/es2015-polyfills.8324bb31dd8aa5f2460c.js" nomodule></script>
 <script type="text/javascript" src="/js/polyfills.8bbb231b43165d65d357.js"></script>
-<script type="text/javascript" src="/js/main.e656574ce4a1a9139c60.js"></script>
+<script type="text/javascript" src="/js/main.0266ecf3b74c220a4d23.js"></script>
 </body>
 </html>
