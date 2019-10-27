213e871ed846fd1e5f647314aca806b479623f99
Willian <willian@codwise.com>
2019-10-26 22:07:36 -0300

    Deploy Frontend

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -8,12 +8,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="/js/favicon.png">
 
-<link rel="stylesheet" href="/js/styles.bce431465f829854801d.css"></head>
+<link rel="stylesheet" href="/js/styles.7aa8f7c3920c5b46dfc7.css"></head>
 <body>
   <app-root></app-root>
 <script type="text/javascript" src="/js/runtime.26209474bfa8dc87a77c.js"></script>
 <script type="text/javascript" src="/js/es2015-polyfills.8324bb31dd8aa5f2460c.js" nomodule></script>
 <script type="text/javascript" src="/js/polyfills.8bbb231b43165d65d357.js"></script>
-<script type="text/javascript" src="/js/main.0266ecf3b74c220a4d23.js"></script>
+<script type="text/javascript" src="/js/main.05fef31eb5f085afc5aa.js"></script>
 </body>
 </html>
