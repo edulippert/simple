79c176df60251f9733c994a5bf38c5fbd4a1b93c
Willian <willian@codwise.com>
2019-12-14 10:06:34 -0300

    Deploy

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -14,5 +14,6 @@
 <script type="text/javascript" src="/js/runtime.26209474bfa8dc87a77c.js"></script>
 <script type="text/javascript" src="/js/es2015-polyfills.8324bb31dd8aa5f2460c.js" nomodule></script>
 <script type="text/javascript" src="/js/polyfills.8bbb231b43165d65d357.js"></script>
-<script type="text/javascript" src="/js/main.d463062dcf0a285ed5d3.js"></script></body>
+<script type="text/javascript" src="/js/main.d463062dcf0a285ed5d3.js"></script>
+</body>
 </html>
